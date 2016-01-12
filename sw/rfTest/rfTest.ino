bb2a3370ca4c3a79b6fa956ec7b6c760c2b6fd94
DaedalusSC <mozingom@gmail.com>
2016-01-11 23:02:31 -0700

    Starting files
    Just a couple of files to get started and create a directory structure.

---
+++ sw/rfTest/rfTest.ino
@@ -0,0 +1,13 @@
+/*
+ * This is a test file to test the RF Rx/Tx path. Creates a simple
+ * 2 way communication channel. 
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
